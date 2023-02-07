942845150c24bd98b477d4885c6923224e31b7a7
Damian Rouson <rouson@lbl.gov>
2023-02-07 15:36:04 -0800

    feat(mp_ml): draft init & inference procedures

--- src/physics/mp_ml.f90
+++ src/physics/mp_ml.f90
@@ -1,51 +1,41 @@
 module mp_ml
-  use assert_m, only : assert
-  use nf, only : network
+  use inference_engine_m, only : inference_engine_t
+  use string_m, only : string_t
+  use sigmoid_m, only : sigmoid_t
+  use file_m, only : file_t
   implicit none
 
   private 
-  public :: ml_init
+  public :: ml_init, ml_mp
 
-  type(network) :: qr_net
+  type(inference_engine_t), allocatable :: networks(:)
 
 contains
 
-  subroutine ml_init(file_name, inweights, aiweights, outweights)
-    character(len=*), intent(in) :: file_name
-    real, allocatable, intent(out) :: inweights(:,:)   ! input layer, e.g., 10 weights/node x 64 nodes
-    real, allocatable, intent(out) :: aiweights(:,:,:) ! hidden layers, e.g., 64 nodes x 64 weights/node x 50 layers
-    real, allocatable, intent(out) :: outweights(:,:)  ! output layer: 1 output x 64 weights (Qr = (kg H20(liq.))/(kg Air))
+  subroutine ml_mp_init(network_files)
+    type(string_t), intent(in) :: network_files(:)
 
-    integer num_inputs, num_nodes, num_layers, num_outputs
-    integer i, j, file_unit, stat
-
-    open(newunit=file_unit, file=file_name, form='formatted', status='old', iostat=stat, action='read')      
-    call assert(stat==0,"stat==0 in open")
-
-    read(file_unit,*,iostat=stat) num_inputs, num_nodes, num_layers, num_outputs
-    call assert(stat==0,"stat==0 in read(...) num_inputs,...")
-
-    allocate(inweights(num_inputs, num_nodes), aiweights(num_nodes, num_nodes, num_layers), outweights(num_outputs, num_nodes))
-
-    do i = 1,size(inweights,1)
-      read(file_unit, *, iostat=stat) inweights(i,:)
-      call assert(stat==0,"stat==0 in read(...) inweights")
-    end do
-
-    do i = 1,size(aiweights,1)
-      do j = 1,size(aiweights,3)
-        read(file_unit, *, iostat=stat) aiweights(i,:,j)
-        call assert(stat==0,"stat==0 in read(...) aiweights")
-      end do
-    end do
-
-    do i = 1, size(outweights,2)
-      read(file_unit, *, iostat=stat) outweights(:,i)
-      call assert(stat==0,"stat==0 in read(...) outweights")
-    end do
-
-    close (file_unit)
-
+    networks = inference_engine_t(file_t(network_files), sigmoid_t())
   end subroutine
 
-end module
+  pure function ml_mp( &
+    qv, qr, qc, ni, th, nr, qs, qg, temp, press, dt_in, &
+    its,ite, jts,jte, kts,kte & ! tile dims
+    ims,ime, jms,jme, kms,kme & ! tile dims
+  ) result(outputs)              
+    integer, intent(in):: ims,ime, jms,jme, kms,kme, its, ite, jts, jte, kts, kte
+    real, dimension(ims:ime, kms:kme, jms:jme), intent(inout):: qv, qr, qc, ni, th, nr, qs, qg, temp, press
+    real, dimension(ims:ime, kms:kme, jms:jme), intent(in):: p
+    real, intent(in):: dt_in
+
+    real outputs(its:ite, jts:jte, size(networks))
+
+    do concurrent(i = its:ite, j = jts:jte, k=1:size(networks))
+      outputs(i,k,j) = networks(k)%infer( &
+        inputs = inputs_t( &
+         qv(i,:,j), qr(i,:,j), qc(i,:,j), ni(i,:,j), th(i,:,j), nr(i,:,j), qs(i,:,j), qg(i,:,j), temp(i,:,j), press(i,:,j), dt_in &
+      ) )
+    end do
+  end function
+
+end module mp_ml
