7d0b6f65c8b68dc6e5d003fa2ce42e7103ac8345
Kiandra WM <kwilsonmorris@sfsu.edu>
2026-06-18 12:14:19 -0700

    Update ICAR visualization script to include a env variable with the path to the directory containing the data to-do: replace hardcoded file name

--- scripts/icar_visualization.py
+++ scripts/icar_visualization.py
@@ -1,9 +1,11 @@
 # IPython log file
 
+import os
 import xarray as xr
 import matplotlib.pyplot as plt
 
-ds=xr.open_dataset("output_simple/icar_out_000001_2000-10-01_00-00-00.nc") # image 000001 domain partition
+data_dir_path=os.environ.get("TRAINING_DATA")
+ds=xr.open_dataset(data_dir_path + "/training_input-image-_100.nc") # temporarily hardcoded file
 print(ds)
 ds["precipitation"][-1].plot() # -1 refers to last time step
 ds["precipitation"][-1].plot(vmax=2) # set max of color scale (for a longer time, try vmax ~ 2000)
