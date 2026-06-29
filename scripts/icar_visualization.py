373a0b96d5666e6d4b3d653089742af4ffc3af5f
Kiandra WM <kwilsonmorris@sfsu.edu>
2026-06-29 13:55:23 -0700

    Update icar_visualization.py: matplotlib image creation changed to create individual images for each loop iteration.

--- scripts/icar_visualization.py
+++ scripts/icar_visualization.py
@@ -15,4 +15,4 @@ for i in range(len(ds.time)):
     plt.clf()
     ds["precipitation"][i].plot(vmax=2)
     plt.pause(0.1) # 0.1 sec pause between frames
-    plt.savefig("file_name.png") # edit this to insert frame number into file name to prevent overwrites 
+    plt.savefig("file_name" + str(i) + ".png")
