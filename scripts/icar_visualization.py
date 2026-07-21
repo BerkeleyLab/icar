22149e9beb93cb75bd146466278428d793fdeb82
Kiandra WM <kwilsonmorris@sfsu.edu>
2026-07-20 17:30:47 -0700

    Update icar vis python script to open multiple data sets

--- scripts/icar_visualization.py
+++ scripts/icar_visualization.py
@@ -5,7 +5,7 @@ import xarray as xr
 import matplotlib.pyplot as plt
 
 data_dir_path=os.environ.get("TRAINING_DATA")
-ds=xr.open_dataset(data_dir_path + "/training_input-image-_100.nc") # temporarily hardcoded file
+ds=xr.open_mfdataset(data_dir_path + "/training_input-image-_*.nc") # temporarily hardcoded file
 print(ds)
 ds["precipitation"][-1].plot() # -1 refers to last time step
 ds["precipitation"][-1].plot(vmax=2) # set max of color scale (for a longer time, try vmax ~ 2000)
