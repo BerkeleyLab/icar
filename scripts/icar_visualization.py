2172a2dedfe2d2d14353c668c0380b4794862068
Kiandra WM <kwilsonmorris@sfsu.edu>
2026-08-31 13:56:25 -0700

    Modify icar_visualization.py to create a mp4 animation.

--- scripts/icar_visualization.py
+++ scripts/icar_visualization.py
@@ -1,18 +1,24 @@
 # IPython log file
 
 import os
-import xarray as xr
+import xarray as xr 
 import matplotlib.pyplot as plt
-
+import matplotlib.animation as animation
+ 
 data_dir_path=os.environ.get("TRAINING_DATA")
 ds=xr.open_mfdataset(data_dir_path + "/training_input-image-_*.nc") # temporarily hardcoded file
 print(ds)
-ds["precipitation"][-1].plot() # -1 refers to last time step
-ds["precipitation"][-1].plot(vmax=2) # set max of color scale (for a longer time, try vmax ~ 2000)
+ds["precipitation"][ -1].plot() # -1 refers to last time step
+#ds["precipitation"][-1].plot(vmax=2) # set max of color scale (for a longer time, try vmax ~ 2000)
 
-# write an image to a file
-for i in range(len(ds.time)):
+
+fig, ax = plt.subplots()
+def func(frame):
     plt.clf()
-    ds["precipitation"][i].plot(vmax=2)
-    plt.pause(0.1) # 0.1 sec pause between frames
-    plt.savefig("file_name" + str(i) + ".png")
+    ds["precipitation"][frame].plot(vmax=2)
+   # plt.pause(0.1) #  0.1 sec pause between frames
+
+#anim = animation.FuncAnimation(fig,func,len(ds.time))
+anim = animation.FuncAnimation(fig,func,60)
+
+anim.save('animation02 .mp4','ffmpeg',30) 
