ee837cce47eb5de5bcc0cd04d97bf232b510c92b
Kiandra WM <kwilsonmorris@sfsu.edu>
2026-09-04 11:11:05 -0700

    Update icar_visualization.py to use an argument for animation file name. Remove redundant plt lines.

--- scripts/icar_visualization.py
+++ scripts/icar_visualization.py
@@ -1,24 +1,21 @@
 # IPython log file
 
 import os
+import sys
 import xarray as xr 
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
- 
+
 data_dir_path=os.environ.get("TRAINING_DATA")
 ds=xr.open_mfdataset(data_dir_path + "/training_input-image-_*.nc") # temporarily hardcoded file
 print(ds)
-ds["precipitation"][ -1].plot() # -1 refers to last time step
-#ds["precipitation"][-1].plot(vmax=2) # set max of color scale (for a longer time, try vmax ~ 2000)
-
 
 fig, ax = plt.subplots()
 def func(frame):
     plt.clf()
-    ds["precipitation"][frame].plot(vmax=2)
-   # plt.pause(0.1) #  0.1 sec pause between frames
+    ds["precipitation"][frame].plot(vmax=2) #vmax sets max of color scale
 
 #anim = animation.FuncAnimation(fig,func,len(ds.time))
 anim = animation.FuncAnimation(fig,func,60)
 
-anim.save('animation02 .mp4','ffmpeg',30) 
+anim.save(sys.argv[1],'ffmpeg',30) 
